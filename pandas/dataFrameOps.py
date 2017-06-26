802e0c5a65207d443fc29582bbcd7be3d3de0f2d
ashwini <ashwini.hadambar@hpe.com>
2017-06-26 12:53:39 +0530

    data slicing - pandas

--- pandas/dataFrameOps.py
+++ pandas/dataFrameOps.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 
 
-""" Data Reader - read from external source - Web"""
+""" Data Reader - read from external source - Web
 
 start = datetime.datetime(2015,1,1)
 end = datetime.datetime(2015,12,31)
@@ -22,9 +22,9 @@ style.use('seaborn-poster')
 
 df.plot()
 plt.show()
+"""
 
-
-""" CREATE DF from dictionary"""
+""" CREATE DF from dictionary
 
 records_dict = {'name':['ashwini','aarti','shweta','shilpa' ],
                 'surname':['barkade','verma','kalalabandi','math'],
@@ -42,19 +42,19 @@ plt.show()
 
 print(records_df.head(2))
 print(records_df.tail(2))
+"""
 
 
-
-""" PANDAS I/O """
+""" PANDAS I/O
 
 boston_df = pd.read_csv("boston.csv")
 print(boston_df.head())
 
 boston_df.to_json("boston.json")
 boston_df.to_html("boston.html")
+ """
 
-
-""" Add delete columns """
+""" Add delete columns 
 
 lst = [1,2,3,4,5,6]
 lst_dataframe = pd.DataFrame(lst)
@@ -65,10 +65,10 @@ lst_dataframe.index = [5,4,3,2,1,0]
 #del lst_dataframe['newCol']
 print(lst_dataframe)
 print(lst_dataframe.columns)
+"""
 
 
-
-""" df properties """
+""" df properties
 
 train_df = pd.read_csv("train.csv")
 print(train_df.dtypes)
@@ -76,10 +76,10 @@ print(train_df.shape)
 print(train_df.head())
 print(train_df.describe())
 print(train_df.info())
+ """
 
 
-
-""" convert category variable to continous variable"""
+""" convert category variable to continous variable
 
 stat_dict = {'name':['a','b','c','d','e','f'],
              'marks': ['good','poor','absent','poor','good','absent']}
@@ -89,9 +89,9 @@ stat_df = pd.DataFrame(stat_dict)
 
 mark_col = stat_df['marks']
 print(mark_col)
+"""
 
-
-""" Method1: Finding unique values from column"""
+""" Method1: Finding unique values from column
 mark_unique = set(mark_col)
 #print(mark_unique)
 
@@ -111,14 +111,16 @@ def convert(x):
 stat_df['marks_cont'] = stat_df['marks'].apply(convert)
 
 print(stat_df)
+"""
 
-"""Method2: Finding unique values in column"""
+
+"""Method2: Finding unique values in column
 makr_unique1 = stat_df['marks'].unique()
 
 print(makr_unique1)
+"""
 
-
-"""Calculating moving average of time series data"""
+"""Calculating moving average of time series data 
 
 data1 = {'data':[1,2,3,4,5,6,7,8,9]}
 
@@ -128,3 +130,28 @@ df = pd.DataFrame(data1)
 rolling_df = df.rolling(2).mean()
 
 print(rolling_df)
+
+plt.plot(df['data'])
+plt.plot(rolling_df['data'])
+plt.show()
+
+"""
+
+""" data selection in pandas"""
+
+df = pd.read_csv("train.csv")
+print(df.head(5))
+
+
+#select single column
+print(df['Pclass'].head())
+
+#select multiple columns
+print(df[['Pclass','Fare']].head())
+
+#select individual record by row index - iloc --> index location
+print(df.iloc[3:5])
+
+#select few cells..same iloc function with first element --> row number, comma seperated by column index
+#example - rows - 0 to 4 cell number - 3
+print(df.iloc[0:5,3])
